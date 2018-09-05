cf96b374c74c73c3abfa3107774af24542494a99
Pramod <ppramod1996@gmail.com>
2018-09-05 20:29:53 +0530

    Array Operations
    Basic logic

---
+++ Array/makePi.java
@@ -0,0 +1,15 @@
+/* makePi
+
+	Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}
+	*/
+
+	public int[] makePi() {
+  
+  int[] intArray=new int[3];
+  intArray[0]=3;
+  intArray[1]=1;
+  intArray[2]=4;
+  return intArray;
+  
+  
+}
